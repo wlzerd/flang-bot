35305fcf916a9dc9162dfd213167d47d56c444d7
wlzerd <64091355+wlzerd@users.noreply.github.com>
2025-06-18 00:07:01 +0900

    update:슬래쉬 커맨드 강제 동기화

--- bot.py
+++ bot.py
@@ -19,7 +19,9 @@ async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     try:
         synced = await bot.tree.sync()
+        GUILD_ID = 850766852148822026
         print(f"Synced {len(synced)} commands")
+        synced = await bot.tree.sync(guild=discord.Object(id=GUILD_ID))
     except Exception as e:
         print(f"Failed to sync commands: {e}")
 
